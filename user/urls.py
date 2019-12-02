c441dbfa8db0be601655322344bfbb2c821c97da
Shourya Sengupta <sengupta.shourya@gmail.com>
2019-12-02 17:29:07 +0530

    registration form

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,6 @@ app_name='user'
 urlpatterns = [
     path('login', views.LoginView.as_view(), name='login'),
     path('logout', views.LogoutView.as_view(), name='logout'),
+    path('register', views.RegisterView.as_view(), name='register'),
+
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
 # Create your views here.
+class RegisterView(CreateView):
+    template_name = 'user/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy("qanda:ask")
